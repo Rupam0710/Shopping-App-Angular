13fb00d807df7df8cb6c337d8d00e47b5bb9ea1b
Rupam <rupampal48@gmail.com>
2023-11-27 14:17:24 +0530

    60cm pass data wth event&property bind(combin) v90

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,7 +7,13 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.scss'
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://asset.slimmingworld.co.uk/content/media/15704/vegetable-jambalaya-new_sw_recipe.jpg?v1=JGXiore20qg9NNIj0tmc3TKfKw-jr0s127JqqpCA2x7sMviNgcAYh1epuS_Lqxebn9V_qusKHfwbF7MOUrAPptzBhXIUL1Xnq2Mmdvx4fOk&width=640&height=640')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://asset.slimmingworld.co.uk/content/media/15704/vegetable-jambalaya-new_sw_recipe.jpg?v1=JGXiore20qg9NNIj0tmc3TKfKw-jr0s127JqqpCA2x7sMviNgcAYh1epuS_Lqxebn9V_qusKHfwbF7MOUrAPptzBhXIUL1Xnq2Mmdvx4fOk&width=640&height=640'),
+    new Recipe('Second Recipe', 'This is simply a test', 'https://asset.slimmingworld.co.uk/content/media/15704/vegetable-jambalaya-new_sw_recipe.jpg?v1=JGXiore20qg9NNIj0tmc3TKfKw-jr0s127JqqpCA2x7sMviNgcAYh1epuS_Lqxebn9V_qusKHfwbF7MOUrAPptzBhXIUL1Xnq2Mmdvx4fOk&width=640&height=640'),
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
